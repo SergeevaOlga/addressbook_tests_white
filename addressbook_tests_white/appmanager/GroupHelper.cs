c3e950e939a1e1be7e41fd3dc4864d69d6b82d35
SergeevaOlga <freyaspb@mail.ru>
2024-06-05 00:05:32 +0300

    White Задание №18: Реализовать тесты для добавления и удаления групп

---
+++ addressbook_tests_white/appmanager/GroupHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.WindowsAPI;
+using TestStack.White.InputDevices;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.TreeItems;
+using TestStack.White.UIItems.WindowItems;
+using System.Windows.Automation;
+using System.Reflection;
+
+
+
+
+namespace addressbook_tests_white
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public static string DELETEGROUPWINTITLE = "Delete group";
+
+        public GroupHelper(ApplicationManager manager) : base(manager)
+        {
+            
+        }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            Window dialogue = OpenGroupsDialogue();
+            Tree tree = dialogue.Get<Tree>("uxAddressTreeView");
+            TreeNode root = tree.Nodes[0];
+            foreach (TreeNode item in root.Nodes)
+            {
+                list.Add(new GroupData()
+                {
+                    Name = item.Text
+                });
+            }
+            CloseGroupsDialogue(dialogue);
+            return list;
+        }
+
+        public void Remove(int index)
+        {
+            Window dialogue = OpenGroupsDialogue();
+            InitGroupRemoving(dialogue, index);
+            SubmitGroupRemoving();
+            CloseGroupsDialogue(dialogue);
+        }
+
+        private void SubmitGroupRemoving()
+        {
+            manager.MainWindow.Get<Button>("uxOKAddressButton").Click();
+        }
+
+        private void InitGroupRemoving(Window dialogue, int index)
+        {
+            Tree tree = dialogue.Get<Tree>("uxAddressTreeView");
+            var item = tree.Nodes[0].Nodes[index];
+
+            item.Click();
+
+            manager.MainWindow.Get<Button>("uxDeleteAddressButton").Click();
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            Window dialogue = OpenGroupsDialogue();
+            dialogue.Get<Button>("uxNewAddressButton").Click();
+            TextBox textBox = (TextBox) dialogue.Get(SearchCriteria.ByControlType(ControlType.Edit));
+            textBox.Enter(newGroup.Name);
+            Keyboard.Instance.PressSpecialKey(KeyboardInput.SpecialKeys.RETURN);
+            CloseGroupsDialogue(dialogue);
+        }
+
+        public Window OpenGroupsDialogue()
+        {
+            manager.MainWindow.Get<Button>("groupButton").Click();
+            return manager.MainWindow.ModalWindow(GROUPWINTITLE);
+        }
+
+        private void CloseGroupsDialogue(Window dialogue)
+        {
+            dialogue.Get<Button>("uxCloseAddressButton").Click();
+        }
+
+        
+    }
+}
---
+++ addressbook_tests_white/tests/GroupRemovalTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_white
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            //app.Groups.OpenGroupsDialogue();
+            GroupData group = new GroupData()
+            {
+                Name = "test"
+            };
+
+            if (app.Groups.GetGroupList().Count <= 1)
+            {
+                app.Groups.Add(group);
+            }
+
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            app.Groups.Remove(0);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            oldGroups.RemoveAt(0);
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
